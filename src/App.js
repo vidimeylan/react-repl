0e1082617a4b6aa5329cda58843fd8f60f5dcf57
Vidi Meylan <vidi_meylan@yahoo.co.id>
2020-09-30 12:44:43 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import {useState} from 'react'
+
+ReactDOM.render(
+  <div className="App">
+    <header className="App-header">
+      <img src="/logo.svg" className="App-logo" alt="logo" />
+      <h3> KCT React Test! </h3>
+      <p>
+        Silakan edit/buat component baru sesuai kebutuhan di soal !
+      </p>
+    </header>
+  </div>,
+  document.getElementById('root')
+);
